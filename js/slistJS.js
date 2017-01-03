cc0a2af4a9ed326a15c14675313e14d01b5c7035
srajsu <steven.gatt@gamesys.co.uk>
2017-01-03 23:14:11 +0100

    more work on date and quantity values
    more work on date and quantity values including JS code for the value
    part

--- js/slistJS.js
+++ js/slistJS.js
@@ -15,6 +15,59 @@
 	var q = localStorage.getItem("SLView");
 	if (q!=null) chosenItems = JSON.parse(q);
 	appendToList();
+	
+		// This button will increment the value
+    $('.qtyplus').click(function(e){
+    
+     		  var qty = $(".qty").val();
+    
+          if(qty => 1){
+	          $(".qtyminus").removeClass("qtyminusGrey");
+          }
+    
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If is not undefined
+        if (!isNaN(currentVal)) {
+            // Increment
+            $('input[name='+fieldName+']').val(currentVal + 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(1);
+        }
+    });
+    // This button will decrement the value till 0
+    $(".qtyminus").click(function(e) {
+    		  var qty = $(".qty").val();
+          if(qty <= 2){
+          $(".qtyminus").addClass("qtyminusGrey");
+          }
+    
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If it isn't undefined or its greater than 0
+        if (!isNaN(currentVal) && currentVal > 1) {
+            // Decrement one
+            $('input[name='+fieldName+']').val(currentVal - 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(1);
+        }
+    });
+    
+    var qty = $(".qty").val();
+    
+    if(qty < 2)
+		{$(".qtyminus").addClass("qtyminusGrey");} 
+	
 	});
 
 
